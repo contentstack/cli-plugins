cc3e47eab52fd434cd58a2d94bcaf89b8c348560
harshitha-cstk <harshitha.d@contentstack.com>
2026-05-15 14:56:34 +0530

    fix: remove deprecated sample and starter apps

--- packages/contentstack-bootstrap/src/config.ts
+++ packages/contentstack-bootstrap/src/config.ts
@@ -15,12 +15,7 @@ export interface AppConfig {
 }
 
 const config: Configuration = {
-  sampleApps: [
-    { displayName: 'React JS (Deprecated)', configKey: 'reactjs' },
-    { displayName: 'Next JS (Deprecated)', configKey: 'nextjs' },
-    { displayName: 'Gatsby (Deprecated)', configKey: 'gatsby' },
-    { displayName: 'Angular (Deprecated)', configKey: 'angular' },
-  ],
+  sampleApps: [],
   starterApps: [
     { displayName: 'Compass App', configKey: 'compass-app' },
     { displayName: 'Kickstart Next.js', configKey: 'kickstart-next' },
@@ -30,15 +25,6 @@ const config: Configuration = {
     { displayName: 'Kickstart Next.js Middleware', configKey: 'kickstart-next-middleware' },
     { displayName: 'Kickstart NuxtJS', configKey: 'kickstart-nuxt' },
     { displayName: 'Kickstart NuxtJS SSR', configKey: 'kickstart-nuxt-ssr' },
-
-    { displayName: 'React JS (Deprecated)', configKey: 'reactjs-starter' },
-    { displayName: 'Next JS (Deprecated)', configKey: 'nextjs-starter' },
-    { displayName: 'Gatsby (Deprecated)', configKey: 'gatsby-starter' },
-    { displayName: 'Angular (Deprecated)', configKey: 'angular-starter' },
-    { displayName: 'Nuxt JS (Deprecated)', configKey: 'nuxt-starter' },
-    { displayName: 'Vue JS (Deprecated)', configKey: 'vue-starter' },
-    { displayName: 'Stencil (Deprecated)', configKey: 'stencil-starter' },
-    { displayName: 'Nuxt3 (Deprecated)', configKey: 'nuxt3-starter' },
   ],
   appLevelConfig: {
     'kickstart-next': {
@@ -74,62 +60,10 @@ const config: Configuration = {
       source: 'contentstack/kickstart-nuxt-ssr',
       stack: 'contentstack/kickstart-stack-seed',
     },
-    nextjs: {
-      source: 'contentstack/contentstack-nextjs-react-universal-demo',
-      stack: 'contentstack/stack-contentstack-nextjs-react-universal-demo',
-    },
-    reactjs: {
-      source: 'contentstack/contentstack-reactjs-universal-sample-app',
-      stack: 'contentstack/stack-contentstack-reactjs-universal-sample-app',
-    },
-    gatsby: {
-      source: 'contentstack/gatsby-starter-contentstack',
-      stack: 'contentstack/stack-gatsby-starter-contentstack',
-    },
-    angular: {
-      source: 'contentstack/contentstack-angular-modularblock-example',
-      stack: 'contentstack/stack-contentstack-angular-modularblock-example',
-    },
     'compass-app': {
       source: 'contentstack/compass-starter-app',
       stack: 'contentstack/compass-starter-stack',
       master_locale: 'en',
-    },
-    'nuxtjs-disabled': {
-      source: 'contentstack/contentstack-nuxtjs-vue-universal-demo',
-      stack: 'shafeeqd959/stack-contentstack-nuxtjs-vue-universal-demo',
-    },
-    'nuxt-starter': {
-      source: 'contentstack/contentstack-nuxtjs-starter-app',
-      stack: 'contentstack/stack-starter-app',
-    },
-    'reactjs-starter': {
-      source: 'contentstack/contentstack-react-starter-app',
-      stack: 'contentstack/stack-starter-app',
-    },
-    'nextjs-starter': {
-      source: 'contentstack/contentstack-nextjs-starter-app',
-      stack: 'contentstack/stack-starter-app',
-    },
-    'gatsby-starter': {
-      source: 'contentstack/contentstack-gatsby-starter-app',
-      stack: 'contentstack/stack-starter-app',
-    },
-    'angular-starter': {
-      source: 'contentstack/contentstack-angular-starter',
-      stack: 'contentstack/stack-starter-app',
-    },
-    'vue-starter': {
-      source: 'contentstack/contentstack-vuejs-starter-app',
-      stack: 'contentstack/stack-starter-app',
-    },
-    'stencil-starter': {
-      source: 'contentstack/contentstack-stencil-starter-app',
-      stack: 'contentstack/stack-starter-app',
-    },
-    'nuxt3-starter': {
-      source: 'contentstack/contentstack-nuxt3-starter-app',
-      stack: 'contentstack/stack-starter-app',
     },
   },
 };
