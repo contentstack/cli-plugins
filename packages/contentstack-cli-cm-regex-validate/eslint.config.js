dc1ec4a9b22cb1cda204ae4f6918e6eb93102979
harshitha-cstk <harshitha.d@contentstack.com>
2026-06-24 13:37:31 +0530

    add eslint.config.js file

---
+++ packages/contentstack-cli-cm-regex-validate/eslint.config.js
@@ -0,0 +1,51 @@
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import oclif from 'eslint-config-oclif';
+import oclifTypescript from 'eslint-config-oclif-typescript';
+
+export default [
+  ...tseslint.configs.recommended,
+
+  oclif,
+
+  oclifTypescript,
+
+  {
+    ignores: [
+      'lib/**/*',
+    ],
+  },
+
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+
+    rules: {
+      'unicorn/prefer-module': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      'unicorn/no-array-for-each': 'off',
+      camelcase: 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'never'],
+      'unicorn/import-style': 'off',
+      'unicorn/prefer-node-protocol': 'off',
+      'unicorn/consistent-function-scoping': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'object-curly-spacing': ['error', 'never'],
+      'node/no-missing-import': 'off',
+    },
+  },
+];
