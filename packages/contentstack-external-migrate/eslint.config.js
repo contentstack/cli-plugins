13ef381b4d693fd1ae4f3b6ec689867ae921ae3d
harshitha-cstk <harshitha.d@contentstack.com>
2026-06-26 11:50:51 +0530

    feat: migrate cli-plugins ESLint configs to flat config format - Convert all .eslintrc files in cli-plugins/packages to eslint.config.js - Remove legacy .eslintrc and .eslintignore files from all packages - Update lint scripts to remove --config .eslintrc flag - Supports ESLint v10.5.0+ flat config system - All 16 packages now use native ES modules flat config format

---
+++ packages/contentstack-external-migrate/eslint.config.js
@@ -0,0 +1,26 @@
+import tseslint from 'typescript-eslint';
+
+export default [
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      'unicorn/prefer-module': 'off',
+      'unicorn/no-abusive-eslint-disable': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      'node/no-missing-import': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      'no-useless-escape': 'off',
+    },
+  },
+];
