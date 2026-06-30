d840c17df7c4fdf284b12e813385b920c5d250ce
raj pandey <raj.pandey@contentstack.com>
2026-06-30 17:34:49 +0530

    fix(flags): normalize short flag consistency across cli-cm-regex-validate, tsgen, migration, and apps-cli

--- packages/contentstack-cli-tsgen/src/commands/tsgen.ts
+++ packages/contentstack-cli-tsgen/src/commands/tsgen.ts
@@ -23,12 +23,12 @@ export default class TypeScriptCodeGeneratorCommand extends Command {
   static description = "Generate TypeScript typings from a Stack";
 
   static examples = [
-    '$ csdx tsgen -a "delivery token alias" -o "contentstack/generated.d.ts"',
-    '$ csdx tsgen -a "delivery token alias" -o "contentstack/generated.d.ts" -p "I"',
-    '$ csdx tsgen -a "delivery token alias" -o "contentstack/generated.d.ts" --no-doc',
-    '$ csdx tsgen -a "delivery token alias" -o "contentstack/generated.d.ts" --include-referenced-entry',
-    '$ csdx tsgen -a "delivery token alias" -o "contentstack/generated.d.ts" --api-type graphql',
-    '$ csdx tsgen -a "delivery token alias" -o "contentstack/generated.d.ts" --api-type graphql --namespace "GraphQL" ',
+    '$ csdx tsgen -a "delivery token alias" --output "contentstack/generated.d.ts"',
+    '$ csdx tsgen -a "delivery token alias" --output "contentstack/generated.d.ts" --prefix "I"',
+    '$ csdx tsgen -a "delivery token alias" --output "contentstack/generated.d.ts" --no-doc',
+    '$ csdx tsgen -a "delivery token alias" --output "contentstack/generated.d.ts" --include-referenced-entry',
+    '$ csdx tsgen -a "delivery token alias" --output "contentstack/generated.d.ts" --api-type graphql',
+    '$ csdx tsgen -a "delivery token alias" --output "contentstack/generated.d.ts" --api-type graphql --namespace "GraphQL" ',
   ];
 
   // Check if a region is a default Contentstack region
@@ -55,7 +55,6 @@ export default class TypeScriptCodeGeneratorCommand extends Command {
     }),
 
     output: flags.string({
-      char: "o",
       description: "full path to output",
       hidden: false,
       multiple: false,
@@ -63,7 +62,6 @@ export default class TypeScriptCodeGeneratorCommand extends Command {
     }),
 
     prefix: flags.string({
-      char: "p",
       description: 'interface prefix, e.g. "I"',
       hidden: false,
       multiple: false,
@@ -72,7 +70,6 @@ export default class TypeScriptCodeGeneratorCommand extends Command {
     }),
 
     doc: flags.boolean({
-      char: "d",
       description: "include documentation comments",
       default: true,
       allowNo: true,
