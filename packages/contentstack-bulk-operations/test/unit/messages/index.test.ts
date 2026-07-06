580c6bee3bc544f08b8f54e89597776f13a36406
harshitha-cstk <harshitha.d@contentstack.com>
2026-07-06 21:52:00 +0530

    fix ES module directory import in messages test

--- packages/contentstack-bulk-operations/test/unit/messages/index.test.ts
+++ packages/contentstack-bulk-operations/test/unit/messages/index.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import messages, { $t } from '../../../src/messages';
+import messages, { $t } from '../../../src/messages/index';
 
 describe('Messages', () => {
   describe('$t function - message interpolation', () => {
